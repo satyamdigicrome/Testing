8ab1cc21ac15a2ae006cc76885b7eeae917b3472
Satyam Shukla <satyam.digicrome@gmail.com>
2026-03-10 16:15:13 +0530

    changes complete

--- database/migrations/2026_02_26_000110_create_course_reviews_table.php
+++ database/migrations/2026_02_26_000110_create_course_reviews_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->string('name')->nullable();
             $table->string('image')->nullable();
             $table->text('text')->nullable();
-            $tabl.e->unsignedTinyInteger('rating')->nullable(); // 1-5
+            $table->unsignedTinyInteger('rating')->nullable(); // 1-5
 
             $table->timestamps();
         });
